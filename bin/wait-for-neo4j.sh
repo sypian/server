8f310c9b817e847524deeee90d1249fd620be71c
Martin Jainta <martin.jainta@gmail.com>
2017-12-09 22:02:32 +0100

    start neo4j server before tests

---
+++ bin/wait-for-neo4j.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+end="$((SECONDS+15))"
+while true; do
+    [[ "200" = "$(curl --silent --write-out "%{http_code}" --output /dev/null http://neo4j:7474)" ]] && echo "neo4j is up and running!" && break
+    [[ "${SECONDS}" -ge "${end}" ]] && echo "Waiting for neo4j timeout of 10 seconds reached!" && exit 1
+    sleep 1
+done
