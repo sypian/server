6eea2695e63aff2a67fd5f1606a5321526320246
Martin Jainta <martin.jainta@gmail.com>
2017-12-10 10:54:52 +0100

    increase timeout for neo4j to 300 seconds

--- bin/wait-for-neo4j.sh
+++ bin/wait-for-neo4j.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-end="$((SECONDS+15))"
+end="$((SECONDS+300))"
 while true; do
     [[ "200" = "$(curl --silent --write-out "%{http_code}" --output /dev/null http://neo4j:7474)" ]] && echo "neo4j is up and running!" && break
-    [[ "${SECONDS}" -ge "${end}" ]] && echo "Waiting for neo4j timeout of 10 seconds reached!" && exit 1
+    [[ "${SECONDS}" -ge "${end}" ]] && echo "Waiting for neo4j timeout of 300 seconds reached!" && exit 1
     sleep 1
 done
