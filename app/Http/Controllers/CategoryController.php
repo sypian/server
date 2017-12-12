8c02a7c356906993a02ba06f76e818c8c45007b9
Martin Jainta <martin.jainta@gmail.com>
2017-12-12 18:17:12 +0100

    use the trait for creating categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,6 +13,14 @@ class CategoryController extends Controller
     /**
      * @return JsonResponse
      */
+    public function createCategory(Request $request): JsonResponse
+    {
+        return $this->createNode('Category', $request);
+    }
+
+    /**
+     * @return JsonResponse
+     */
     public function getCategory(Request $request): JsonResponse
     {
         return $this->getNode('Category', $request);
@@ -32,47 +40,5 @@ class CategoryController extends Controller
     public function deleteCategory(Request $request): JsonResponse
     {
         return $this->deleteNode('Category', $request);
-    }
-
-    /**
-     * @return JsonResponse
-     */
-    public function createCategory(Request $request): JsonResponse
-    {
-        if ($request->has('name') && count($request->except('name')) === 0) {
-            $category = new Category();
-            $category->setName($request->get('name'));
-
-            $entityManager = app()->make('Neo4j\EntityManager');
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return response()->json(['message' => $category->getId()], 200);
-        }
-
-        return $this->getErrorResponseOnCreate($request);
-    }
-
-    /**
-     * @return JsonResponse
-     */
-    protected function getErrorResponseOnCreate(Request $request): JsonResponse
-    {
-        if (count($request->all()) === 0) {
-            return response()->json(['message' => 'Empty request.'], 405);
-        } elseif (!$request->has('name')) {
-            return response()->json(['message' => 'No category name defined.'], 405);
-        } elseif (count($request->except('name')) > 0) {
-            $invalidParams = array_keys($request->except('name'));
-
-            if (count($invalidParams) > 1) {
-                return response()->json(
-                    ['message' => sprintf('Properties %s not supported.', $this->getFormattedParams($invalidParams))],
-                    405
-                );
-            }
-        }
-
-        return response()->json(['message' => 'Property "'.reset($invalidParams).'" not supported.'], 405);
     }
 }
--- tests/Integration/CategoryTest.php
+++ tests/Integration/CategoryTest.php
@@ -17,8 +17,9 @@ class CategoryTest extends TestCase
         $query = $entityManager->createQuery('MATCH (n) DETACH DELETE n');
         $query->execute();
     }
+
     /**
-     * Returns a 200 for a correct post call.
+     * Returns a 201 for a correct post call.
      *
      * @return void
      */
@@ -27,23 +28,21 @@ class CategoryTest extends TestCase
         $this->json('POST', '/category', ['name' => 'testcat']);
 
         $this->assertEquals(
-            200,
+            201,
             $this->response->getStatusCode()
         );
     }
 
     /**
-     * Returns a 405 for an invalid input.
+     * Returns a 405 if we dont pass a name.
      *
      * @return void
-     *
-     * @dataProvider getInvalidPostInputs
      */
-    public function testInvalidPostInputs($input, $responseContent)
+    public function testCreateWithoutName()
     {
-        $this->json('POST', '/category', $input)
+        $this->json('POST', '/category', ['nameeee' => 'testcat'])
             ->seeJson([
-                'message' => $responseContent,
+                'message' => 'No Category name defined.',
             ]);
 
         $this->assertEquals(
@@ -53,28 +52,22 @@ class CategoryTest extends TestCase
     }
 
     /**
-     * @return mixed[]
+     * Returns a 409 if the project to create already exists.
+     *
+     * @return void
      */
-    public function getInvalidPostInputs(): array
+    public function testCreateDuplicate()
     {
-        return [
-            [
-                ['keyyy' => 'testcat', 'falseproperty' => 'test'],
-                'No category name defined.',
-            ],
-            [
-                ['name' => 'testcat', 'falseproperty' => 'test'],
-                'Property "falseproperty" not supported.',
-            ],
-            [
-                ['name' => 'testcat', 'falseproperty1' => 'test', 'falseproperty2' => 'test'],
-                'Properties "falseproperty1", "falseproperty2" not supported.',
-            ],
-            [
-                [],
-                'Empty request.',
-            ]
-        ];
+        $this->json('POST', '/category', ['name' => 'testcat']);
+        $this->json('POST', '/category', ['name' => 'testcat'])
+            ->seeJson([
+                'message' => 'Category with name "testcat" already exists.',
+            ]);
+
+        $this->assertEquals(
+            409,
+            $this->response->getStatusCode()
+        );
     }
 
     public function testValidGetRequest()
