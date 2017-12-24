3d0103ce6ac5309a5dbbe83edacfadc3896c6d7b
Martin Jainta <martin.jainta@gmail.com>
2017-12-24 21:03:34 +0100

    add helper functionality to handle json payload in the controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    protected $payload = [];
+    protected $errors = [];
+
     /**
      * Returns the given strings formatted in double quotes and separated by a comma and space.
      *
@@ -20,5 +24,48 @@ class Controller extends BaseController
         }
 
         return trim($paramStr, ', ');
+    }
+
+    /**
+     * Adds a key/value pair to the json payload.
+     *
+     * @param mixed $value Any value to be saved.
+     * @param string $key The key as a string to be used. A second call with the same key will overwrite the
+     * previous one.
+     */
+    public function addToPayload($value, string $key = 'message')
+    {
+        $this->payload[$key] = $value;
+    }
+
+    public function addError(string $message)
+    {
+        $this->errors[] = [
+            'message' => $message,
+        ];
+    }
+
+    /**
+     * Generates and returns a json response using the set payload and errors.
+     *
+     * addtoPayload() and addError() functions should be used befor calling this.
+     *
+     * @param int $statusCode The status code to be used for the response.
+     */
+    public function generateJsonResponse(int $statusCode): JsonResponse
+    {
+        $responseJson = $this->payload;
+        $responseJson['errors'] = $this->errors;
+        return response()->json($responseJson, $statusCode);
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
--- tests/Controllers/ControllerTest.php
+++ tests/Controllers/ControllerTest.php
@@ -29,4 +29,81 @@ class ControllerTest extends TestCase
             ],
         ];
     }
+
+    public function testGetEmptyErrors()
+    {
+        $controller = new Controller();
+        $this->assertEquals([], $controller->getErrors());
+    }
+
+    public function testAddError()
+    {
+        $controller = new Controller();
+        $controller->addError('generic error message');
+        $this->assertEquals([['message' => 'generic error message']], $controller->getErrors());
+    }
+
+    public function testAddErrors()
+    {
+        $controller = new Controller();
+        $controller->addError('generic error message1');
+        $controller->addError('generic error message2');
+        $this->assertEquals([
+            ['message' => 'generic error message1'],
+            ['message' => 'generic error message2']
+        ], $controller->getErrors());
+    }
+
+    public function testGetEmptyPayload()
+    {
+        $controller = new Controller();
+        $this->assertEquals([], $controller->getPayload());
+    }
+
+    public function testAddMessage()
+    {
+        $controller = new Controller();
+        $controller->addToPayload('generic test message', 'message');
+        $this->assertEquals(['message' => 'generic test message'], $controller->getPayload());
+    }
+
+    public function testAssumeMessageAsPayloadKey()
+    {
+        $controller = new Controller();
+        $controller->addToPayload('generic test message');
+        $this->assertEquals(['message' => 'generic test message'], $controller->getPayload());
+    }
+
+    public function testAddMultipleData()
+    {
+        $controller = new Controller();
+        $controller->addToPayload('generic test message', 'message');
+        $controller->addToPayload(123, 'number');
+        $this->assertEquals(['message' => 'generic test message','number' => 123], $controller->getPayload());
+    }
+
+    public function testOverwritePayloadKey()
+    {
+        $controller = new Controller();
+        $controller->addToPayload('generic test message', 'message');
+        $controller->addToPayload('generic test message overwrite', 'message');
+        $this->assertEquals(['message' => 'generic test message overwrite'], $controller->getPayload());
+    }
+
+    public function testGenerateJsonResponse()
+    {
+        $controller = new Controller();
+        $controller->addToPayload('generic test message', 'message');
+        $controller->addError('generic error message1');
+        $controller->addError('generic error message2');
+        $response = $controller->generateJsonResponse(500);
+        $this->assertEquals([
+            'message' => 'generic test message',
+            'errors' => [
+                ['message' => 'generic error message1'],
+                ['message' => 'generic error message2'],
+            ]
+        ], $response->getData(true));
+        $this->assertEquals(500, $response->getStatusCode());
+    }
 }
